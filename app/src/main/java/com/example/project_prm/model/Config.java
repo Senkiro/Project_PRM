48d362965c25ea64c0f114536e3bba720764d0aa
Senkiro <101233448+Senkiro@users.noreply.github.com>
2024-03-12 22:18:22 +0700

    first commit

---
+++ app/src/main/java/com/example/project_prm/model/Config.java
@@ -0,0 +1,6 @@
+package com.example.project_prm.model;
+
+public class Config {
+    public static final String EMAIL ="vinhnthe163441@fpt.edu.vn";
+    public static final String PASSWORD ="jtss wjvj xnrc vstj";
+}
